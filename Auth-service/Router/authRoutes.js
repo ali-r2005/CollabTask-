2980960de6d42c3192b60481f13bbf4422b3df21
ali rami <ali.rami.6699@gmail.com>
2025-03-21 01:35:50 +0000

    authentification

---
+++ Auth-service/Router/authRoutes.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const router = express.Router();
+
+const userAdmin = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization');
+        if (!token) return res.status(403).json({ message: "Access denied. No token provided." });
+        const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+        if (decoded.role !== 'admin') return res.status(403).json({ message: "Access denied. Admin resources!" });
+        next();
+    }catch (err) {
+            res.status(401).json({ message: "Invalid token" });
+        }
+    };
+
+const authenticateToken = (req, res, next) => {
+        const token = req.header('Authorization');
+      
+        if (!token) return res.status(403).json({ message: "Access denied. No token provided." });
+      
+        try {
+            const decoded = jwt.verify(token.split(" ")[1], SECRET_KEY);
+            req.user = decoded;
+            next(); // Proceed to the next middleware or route
+        } catch (err) {
+            res.status(401).json({ message: "Invalid token" });
+        }
+      };
+
+router.post('/register', async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
+        let user = await User.findOne({ email });
+
+        if (user) return res.status(400).json({ message: "Utilisateur déjà existant" });
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        user = new User({ name, email, password: hashedPassword, role });
+        await user.save();
+
+        res.status(201).json({ message: "Utilisateur créé avec succès" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "Email ou mot de passe incorrect" });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: "Email ou mot de passe incorrect" });
+
+        if (user.isBlocked) return res.status(403).json({ message: "Votre compte est bloqué" });
+
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '7h' });
+
+        res.json({ token, user: { id: user._id, name: user.name, email: user.email, role: user.role } });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+router.put('/block/:id',userAdmin, authenticateToken , async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return res.status(404).json({ message: "Utilisateur non trouvé" });
+
+        user.isBlocked = !user.isBlocked;
+        await user.save();
+
+        res.json({ message: `Utilisateur ${user.isBlocked ? 'bloqué' : 'débloqué'}` });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.get("/user/:id" , userAdmin , authenticateToken, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return res.status(404).json({ message: "Utilisateur non trouvé" });
+
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.put("/user/:id", userAdmin, authenticateToken, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return res.status(404).json({ message: "Utilisateur non trouvé" });
+
+        const { name, email, role } = req.body;
+        user.name = name;
+        user.email = email;
+        user.role = role;
+        await user.save();
+
+        res.json({ message: "Utilisateur modifié avec succès" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.delete("/user/:id", userAdmin, authenticateToken, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return res.status(404).json({ message: "Utilisateur non trouvé" });
+
+        await user.remove();
+
+        res.json({ message: "Utilisateur supprimé avec succès" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.get('/users', authenticateToken , async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
---
+++ Auth-service/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connecté"))
+  .catch(err => console.error("Erreur MongoDB :", err));
+
+app.use('/auth', require('./Router/authRoutes')); 
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Serveur d'authentification démarré sur http://localhost:${PORT}`);
+});
